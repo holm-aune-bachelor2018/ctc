3e4abb26706c1a9475e4bf3f5cc35ddcb26530f9
Marit Sundet Holm <marith1@stud.ntnu.no>
2018-04-03 15:37:33 +0200

    Comments and cleanup

--- preprocessing.py
+++ preprocessing.py
@@ -12,56 +12,64 @@ def generate_batch(df, frame_length, hop_length, mfcc_features=12):
     :param df: Dataframes contaning (wav_filename, wav_filesize, transcript)
     :param frame_length: Length of each frame generated.
     :param hop_length: How far to jump for each frame.
-    :return: dataX: (batch_size * max_timesteps * mfcc_features) dim matrix,
-             dataY: (batch_size * max_transcript_length) dim matrix
+    :param mfcc_features:
+    :return:    x_data:     dim(batch_size * max_timesteps * mfcc_features)
+                y_data:     dim(batch_size * max_transcript_length)
+                len_x_seq:  dim(batch_size * 1) length of each sequence (numb of frames) in x_data
+                len_y_seq:  dim(batch_size * 1) length of each sequence (numb of letters) in y_data
     """
-    print "\nIs null: \n", df.isnull().any(), "\n"
 
-    # Fetch lagrest wav_file to find the longest sequence length in batch
+    # print "\nIs null: \n", df.isnull().any(), "\n"
+
+    # Fetch largest wav_file to find the longest sequence length in batch
     largest_index = df['wav_filesize'].idxmax()
-    #print 'Largest ind: ', largest_index
     path_to_largest = df.loc[largest_index]['wav_filename']
-    #print "path to largest: ", path_to_largest
     max_length = get_seq_size(path_to_largest, frame_length, hop_length)
-    #print "Length (in frames) of largest wav_file: ", max_length
 
-    dataX = np.empty([0, max_length, mfcc_features])
-    x_lengths = []
-    listY = []
-    y_lengths = []
+    # print 'Largest ind: ', largest_index
+    # print "path to largest: ", path_to_largest
+    # print "Length (in frames) of largest wav_file: ", max_length
 
+    # Initializing vectors
+    x_data = np.empty([0, max_length, mfcc_features])
+    y_data_unpadded = []
+    len_x_seq = []
+    len_y_seq = []
 
     for i in range(0, df.shape[0]):
-        # dataY: y_txt from string to integer
+        # y_data: y_txt from string to integer
         y_txt = df.iloc[i]['transcript']
         y_int = data.text_to_int_sequence(y_txt)
-        y_lengths.append(len(y_int))
-        listY.append(y_int)
+        y_data_unpadded.append(y_int)
 
-        # dataX: extract mfcc features and pad so every frame-sequence is equal length
+        len_y_seq.append(len(y_int))
+
+        # x_data: extract mfcc features and pad so every frame-sequence is equal length
         path = df.iloc[i]['wav_filename']
         x, x_len = mfcc(path, frame_length, hop_length, max_length, mfcc_features=mfcc_features)
-        x_lengths.append(x_len)
-        dataX = np.insert(dataX, i, x, axis=0)
+        x_data = np.insert(x_data, i, x, axis=0)
 
-    y_length = len(max(listY, key=len))
-    # print 'Y-length: ',y_length
+        len_x_seq.append(x_len)
 
+    # Finds longest sequence in y
+    y_length = len(max(y_data_unpadded, key=len))
+    y_data = np.empty([0, y_length])
+    # print 'Y-length: ', y_length
 
-    dataY = np.empty([0, y_length])
+    # Pads every sequence in y to be equal length
     for i in range(0, df.shape[0]):
-        y = listY[i]
+        y = y_data_unpadded[i]
+
         for j in range(len(y), y_length):
             y.append(0)
 
-        dataY = np.insert(dataY, i, y, axis=0)
+        y_data = np.insert(y_data, i, y, axis=0)
 
+    # print "DataX shape: ", x_data.shape
+    # plot_mfcc(x_data.T)
+    # print "y_data: \n", y_data
 
-    # print "DataX shape: ", dataX.shape
-    # plot_mfcc(dataX.T)
-    # print "dataY: \n", dataY
-
-    return dataX, dataY, x_lengths, y_lengths
+    return x_data, y_data, len_x_seq, len_y_seq
 
 
 def mfcc(file_path, frame_length, hop_length, max_pad_length, mfcc_features=12):
@@ -72,7 +80,9 @@ def mfcc(file_path, frame_length, hop_length, max_pad_length, mfcc_features=12):
     :param frame_length: length of each frame generated
     :param hop_length: how far to jump for each frame
     :param max_pad_length: length (number of frames) of longest sequence
-    :return: Padded 2d array with time_steps * input_dim (mfcc features)
+    :param mfcc_features: number of mfcc-features to extract
+    :return: mfcc_frames: Padded 2d array with time_steps * input_dim (mfcc features)
+             x_length: length of sequence before padding
     """
     y, sr = librosa.load(file_path, sr=None)
 
@@ -95,16 +105,17 @@ def plot_mfcc(mfcc_frames):
     plt.show()
 
 
-def get_seq_size(path, frame_length, hop_length):
+def get_seq_size(path, frame_length, hop_length, mfcc_features=12):
     """
     Calculates the number of frames (time-steps) for a given wav_file.
     :param path: path to wav-file
     :param frame_length: length of each frame generated
     :param hop_length: how far to jump for each frame
+    :param mfcc_features: number of mfcc-features to extract
     :return: number of frames/sequence length
     """
 
     y, sr = librosa.load(path, sr=None)
-    mfcc = librosa.feature.mfcc(y, sr, n_fft=frame_length, hop_length=hop_length, n_mfcc=12)
+    mfcc = librosa.feature.mfcc(y, sr, n_fft=frame_length, hop_length=hop_length, n_mfcc=mfcc_features)
 
     return mfcc.shape[1]
--- train.py
+++ train.py
@@ -1,12 +1,10 @@
-from keras.models import Model
 import nn_models
 import preprocessing
 import numpy as np
 from keras import optimizers
-import pandas as pd
 import data
 
-#Preprocessing
+# Preprocessing
 path = "sample_data/wav_sample/sample_librivox-dev-clean.csv"
 frequency = 16
 frame_length = 20*frequency
@@ -15,14 +13,13 @@ hop_length = 10*frequency
 dataprop, df_final = data.combine_all_wavs_and_trans_from_csvs(path)
 dataX, dataY, x_length, y_length = preprocessing.generate_batch(df_final, frame_length, hop_length)
 
-df_final.to_csv('data.csv')
+# df_final.to_csv('data.csv')
 
 
 # Model
-
-units = 512                         #numb of hidden nodes
-mfcc_features = 12                  #input_dim
-input_shape=(None, mfcc_features)   #None to be able to process batches of any size
+units = 512                         # numb of hidden nodes
+mfcc_features = 12                  # input_dim
+input_shape=(None, mfcc_features)   # "None" to be able to process batches of any size
 
 np.random.seed(7)
 loss = {'ctc': lambda y_true, y_pred: y_pred}
@@ -33,40 +30,30 @@ n_batch = len(dataX)
 n_epoch = 1
 
 
-brnn_model = nn_models.dnn_brnn(units,)
+brnn_model = nn_models.dnn_brnn(units)
 brnn_model.compile(loss=loss, optimizer=optimizer, metrics=metrics)
 brnn_model.summary()
 
 #dataX
 # x_data
 # y_true - (samples, max_string_length) containing the truth labels.
-# input_length - (samples, 1) containing the sequence length for each batch item in y_pred = ???
-# label_length - (samples, 1) containing the sequence length for each batch item in y_true = 132
+# input_length - (samples, 1) containing the sequence length for each batch item in y_pred
+# label_length - (samples, 1) containing the sequence length for each batch item in y_true
 
 # CTC requirement: label_length must be shorter than the input_length
-
 
 x_data = dataX                        # batch_size * time_steps * features
 y_true = dataY                        # batch_size * max_string_length
 input_length = np.array(x_length)     # batch_size * 1
 label_length = np.array(y_length)     # batch_size * 1
 
-
-
-
-print "\Before fitting: "
+print "\nBefore fitting: "
 print "x_data shape: ", x_data.shape
 print "y_data shape: ", y_true.shape
 print "input_length shape: ", input_length.shape
 print "label_length shape: ", label_length.shape, "\n"
 
 
-#print "x_data: \n", x_data
-#print "y_true: \n", y_true
-#print "label length: \n", label_length
-#print "input length: \n", input_length
-
-#y_true, y_pred, input_length, label_length
 for i in range(n_epoch):
     brnn_model.fit([x_data, y_true, input_length, label_length], dataY, epochs=n_epoch, batch_size=n_batch, verbose=2)
     brnn_model.reset_states()
