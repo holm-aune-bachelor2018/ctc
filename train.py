621665654b1060c56213150eacd18bc5a397a3c7
Marit Sundet Holm <marith1@stud.ntnu.no>
2018-04-20 11:22:31 +0200

    added print of model and network parameters

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
-#!$HOME/envs/tensorflow-workq/bin/python
+#!/home/anitakau/envs/tensorflow-workq/bin/python
+#!/home/marith1/envs/tensorflow/bin/python
 
 import sys
 import nn_models
@@ -12,34 +13,38 @@ import tensorflow as tf
 
 
 # Preprocessing
-path = "$HOME/ctc/data_dir/librivox-dev-clean.csv"
-path_validation = "$HOME/ctc/data_dir/librivox-test-clean.csv"
+path = "/home/<user>/ctc/data_dir/librivox-dev-clean.csv"
+path_validation = "/home/<user>/ctc/data_dir/librivox-test-clean.csv"
 
-_, input_dataframe = data.combine_all_wavs_and_trans_from_csvs(path)
+print "Reading training data:"
+data_properties, input_dataframe = data.combine_all_wavs_and_trans_from_csvs(path)
+print "\n Reading validation data: "
 _, validation_df = data.combine_all_wavs_and_trans_from_csvs(path_validation)
 
-#input_dataframe.to_csv('data.csv')
-#validation_df.to_csv('validdata.csv')
+# input_dataframe.to_csv('data.csv')
+# validation_df.to_csv('validdata.csv')
 
+# sampling rate of data in khz (LibriSpeech is 16khz)
 frequency = 16
 
 # Parameters for script
-# batch_size, mfcc_features, epoch_length, epochs, units, model_name
+# batch_size, mfcc_features, epoch_length, epochs, units, learning_rate, model_name
 
-batch_size = int(sys.argv[1])
-mfcc_features = int(sys.argv[2])
-epoch_length = int(sys.argv[3])
+batch_size = int(sys.argv[1])                       # Number of files in one batch
+mfcc_features = int(sys.argv[2])                    # Number of mfcc features (per frame) to extract
+input_epoch_length = int(sys.argv[3])               # Number of batches per epoch (if 0, trains on full dataset)
 
 # Model specifications
 epochs = int(sys.argv[4])                           # Number of epochs
 units = int(sys.argv[5])                            # Number of hidden nodes
-model_name = sys.argv[6]                            # path to save model
+learning_rate = int(sys.argv[6])                    # Learning rate
+model_name = sys.argv[7]                            # path to save model
 
 params = {'batch_size': batch_size,
           'frame_length': 20 * frequency,
           'hop_length': 10 * frequency,
           'mfcc_features': mfcc_features,
-          'epoch_length': epoch_length        # number of batches per epoch (for testing)
+          'epoch_length': input_epoch_length
 }
 '''
 params = {'batch_size': 12,        
@@ -59,8 +64,14 @@ output_dim = 29                                     # output dimension (n-1)
 eps = 1e-8                                          # epsilon 1e-8
 learning_rate = 0.001
 optimizer = optimizers.Adam(lr=learning_rate, epsilon=eps, clipnorm=2.0)
-metrics = []
-# 'accuracy'
+
+calc_epoch_length = training_generator.__len__()
+
+print "\n\n Model and training parameters: "
+print " - epochs: ", epochs, "\n - batch size: ", batch_size, \
+      "\n - input epoch length: ", input_epoch_length, "\n - network epoch length: ", calc_epoch_length, \
+      "\n - training on ",calc_epoch_length*batch_size," files","\n - learning rate: ", learning_rate,\
+      "\n - hidden units: ", units, "\n - mfcc features: ", mfcc_features, "\n"
 
 # loss function to compile model, actual CTC loss function defined as a lambda layer in model
 loss = {'ctc': lambda y_true, y_pred: y_pred}
