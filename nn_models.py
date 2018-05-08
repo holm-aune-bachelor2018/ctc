53bbb95edd2215c164e75a9054f4801ae04c3855
Marit Sundet Holm <marith1@stud.ntnu.no>
2018-05-08 13:32:46 +0200

    Added cnn_rnn model

--- nn_models.py
+++ nn_models.py
@@ -1,6 +1,6 @@
 from keras.models import Model
-from keras.layers import Dense, SimpleRNN, LSTM, CuDNNLSTM, Bidirectional, Masking, TimeDistributed,\
-    Lambda, Input, Dropout
+from keras.layers import Dense, SimpleRNN, LSTM, CuDNNLSTM, Bidirectional, TimeDistributed, Conv1D, ZeroPadding1D
+from keras.layers import Lambda, Input, Dropout, Masking, Flatten
 from keras import backend as K
 
 
@@ -15,9 +15,11 @@ def model(model_type='default', units=512, input_dim=26, output_dim=29, dropout=
     elif model_type == 'deep_rnn':
         network_model = deep_rnn(units, input_dim, output_dim, dropout)
 
+    elif model_type == 'cnn_brnn':
+        network_model = cnn_brnn(units, input_dim, output_dim, dropout)
+
     else:
-        print "Not a valid model."
-        return 0
+        raise ValueError("Not a valid model: ", model_type)
 
     return network_model
 
@@ -96,6 +98,84 @@ def dnn_brnn(units, input_dim=26, output_dim=29, dropout=0.2):
 
     return network_model
 
+
+def cnn_brnn(units, input_dim=26, output_dim=29, dropout=0.2):
+    """
+    :param units: units
+    :param input_dim: input_dim(mfcc_features)
+    :param output_dim output dim of final layer of model, input to CTC layer
+    :return: dnn_brnn model
+
+    Model contains:
+     1 layer of masking
+     3 layers of fully connected clipped ReLu (DNN) with dropout 20 % between each layer
+     1 layer of BRNN
+     1 layers of fully connected clipped ReLu (DNN) with dropout 20 % between each layer
+     1 layer of softmax
+    """
+
+    dtype = 'float32'
+    channels = 32
+    # kernel and bias initializers for fully connected dense layers
+    kernel_init_dense = 'random_normal'
+    bias_init_dense = 'random_normal'
+
+    # kernel and bias initializers for recurrent layer
+    kernel_init_conv = 'glorot_uniform'
+    bias_init_conv = 'zeros'
+
+    # kernel and bias initializers for recurrent layer
+    kernel_init_rnn = 'glorot_uniform'
+    bias_init_rnn = 'zeros'
+
+    # ---- Network model ----
+    # x_input layer, dim: (batch_size * x_seq_size * features)
+    input_data = Input(name='the_input',shape=(None, input_dim), dtype=dtype)
+
+    # (batch_size * x_seq_size * mfcc_features)
+    # (batch_size, steps, input_dim)
+
+    # Masking layer
+    # x = Masking(mask_value=0., name='masking')(input_data)
+
+    x = ZeroPadding1D(padding=(0, 1024))(input_data)
+
+    x = Conv1D(filters=units, kernel_size=5, strides=1, activation=None,
+               kernel_initializer=kernel_init_conv, bias_initializer=bias_init_conv)(x)
+
+    x = Conv1D(filters=units, kernel_size=5, strides=1, activation=None,
+               kernel_initializer=kernel_init_conv, bias_initializer=bias_init_conv)(x)
+
+    x = Conv1D(filters=units, kernel_size=5, strides=2, activation=None,
+               kernel_initializer=kernel_init_conv, bias_initializer=bias_init_conv)(x)
+
+    # Bidirectional RNN (with ReLu)
+    x = Bidirectional(SimpleRNN(units, activation='relu', kernel_initializer=kernel_init_rnn, dropout=0.2,
+                                bias_initializer=bias_init_rnn, return_sequences=True),
+                      merge_mode='concat', name='bi_rnn')(x)
+
+    # 1 fully connected relu layer
+    x = TimeDistributed(Dense(units=units, kernel_initializer=kernel_init_dense, bias_initializer=bias_init_dense,
+                              activation='relu'), name='fc_4')(x)
+    x = TimeDistributed(Dropout(dropout), name='dropout_4')(x)
+
+    # Output layer with softmax
+    y_pred = TimeDistributed(Dense(units=output_dim, kernel_initializer=kernel_init_dense,
+                                   bias_initializer=bias_init_dense, activation='softmax'), name='softmax')(x)
+
+    # ---- CTC ----
+    # y_input layers (transcription data) for CTC loss
+    labels = Input(name='the_labels', shape=[None], dtype=dtype)        # transcription data (batch_size * y_seq_size)
+    input_length = Input(name='input_length', shape=[1], dtype=dtype)   # unpadded len of all x_sequences in batch
+    label_length = Input(name='label_length', shape=[1], dtype=dtype)   # unpadded len of all y_sequences in batch
+
+    # Lambda layer with ctc_loss function due to Keras not supporting CTC layers
+    loss_out = Lambda(function=ctc_lambda_func, name='ctc', output_shape=(1,))\
+                     ([y_pred, labels, input_length, label_length])
+
+    network_model = Model(inputs=[input_data, labels, input_length, label_length], outputs=loss_out)
+
+    return network_model
 
 def deep_rnn(units, input_dim=26, output_dim=29, dropout=0.2, numb_of_rnn=3):
     """
