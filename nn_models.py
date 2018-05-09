7d68bcf8695f94af22706986329d654b86a8b8d5
anitakau <anitakau@stud.ntnu.no>
2018-05-09 14:38:21 +0200

    Deep LSTM model

--- nn_models.py
+++ nn_models.py
@@ -18,6 +18,9 @@ def model(model_type='default', units=512, input_dim=26, output_dim=29, dropout=
     elif model_type == 'cnn_brnn':
         network_model = cnn_brnn(units, input_dim, output_dim, dropout)
 
+    elif model_type == 'deep_lstm':
+        network_model = deep_lstm(units, input_dim, output_dim, dropout)
+
     else:
         raise ValueError("Not a valid model: ", model_type)
 
@@ -318,6 +321,79 @@ def dnn_blstm(units, input_dim=26, output_dim=29, dropout=0.2):
         x = Bidirectional(LSTM(units, activation='relu', kernel_initializer=kernel_init_rnn, dropout=dropout,
                                bias_initializer=bias_init_rnn, unit_forget_bias=True, return_sequences=True),
                           merge_mode='sum', name='bi_lstm')(x)
+
+    # 1 fully connected relu layer
+    x = TimeDistributed(Dense(units=units, kernel_initializer=kernel_init_dense, bias_initializer=bias_init_dense,
+                              activation='relu'), name='fc_4')(x)
+    x = TimeDistributed(Dropout(dropout), name='dropout_4')(x)
+
+    # Output layer with softmax
+    y_pred = TimeDistributed(Dense(units=output_dim, kernel_initializer=kernel_init_dense,
+                                   bias_initializer=bias_init_dense, activation='softmax'), name='softmax')(x)
+
+    # ---- CTC ----
+    # y_input layers (transcription data) for CTC loss
+    labels = Input(name='the_labels', shape=[None], dtype=dtype)  # transcription data (batch_size * y_seq_size)
+    input_length = Input(name='input_length', shape=[1], dtype=dtype)  # unpadded len of all x_sequences in batch
+    label_length = Input(name='label_length', shape=[1], dtype=dtype)  # unpadded len of all y_sequences in batch
+
+    # Lambda layer with ctc_loss function due to Keras not supporting CTC layers
+    loss_out = Lambda(function=ctc_lambda_func, name='ctc', output_shape=(1,))\
+                     ([y_pred, labels, input_length, label_length])
+
+    network_model = Model(inputs=[input_data, labels, input_length, label_length], outputs=loss_out)
+
+    return network_model
+
+
+def deep_lstm(units, input_dim=26, output_dim=29, dropout=0.2):
+    """
+        :param units: units
+        :param input_dim: input_dim(mfcc_features)
+        :param output_dim output dim of final layer of model, input to CTC layer
+        :return: dnn_brnn model
+
+        Model contains:
+         3 layers of fully connected clipped ReLu (DNN) with dropout 20 % between each layer
+         3 layers LSTM
+         1 layers of fully connected clipped ReLu (DNN) with dropout 20 % between each layer
+         1 layer of softmax
+        """
+
+    dtype = 'float32'
+    # kernel and bias initializers for fully connected dense layers
+    kernel_init_dense = 'random_normal'
+    bias_init_dense = 'random_normal'
+
+    # kernel and bias initializers for recurrent layer
+    kernel_init_rnn = 'glorot_uniform'
+    bias_init_rnn = 'zeros'
+
+    # ---- Network model ----
+    # x_input layer, dim: (batch_size * x_seq_size * mfcc_features)
+    input_data = Input(name='the_input', shape=(None, input_dim), dtype=dtype)
+
+    # CuDNNLSTM does not support masking
+    x = input_data
+
+    # 3 fully connected layers DNN ReLu
+    # Dropout rate 20 % at each FC layer
+    x = TimeDistributed(Dense(units=units, kernel_initializer=kernel_init_dense, bias_initializer=bias_init_dense,
+                              activation=clipped_relu), name='fc_1')(x)
+    x = TimeDistributed(Dropout(dropout), name='dropout_1')(x)
+
+    x = TimeDistributed(Dense(units=units, kernel_initializer=kernel_init_dense, bias_initializer=bias_init_dense,
+                              activation=clipped_relu), name='fc_2')(x)
+    x = TimeDistributed(Dropout(dropout), name='dropout_2')(x)
+
+    x = TimeDistributed(Dense(units=units, kernel_initializer=kernel_init_dense, bias_initializer=bias_init_dense,
+                              activation=clipped_relu), name='fc_3')(x)
+    x = TimeDistributed(Dropout(dropout), name='dropout_3')(x)
+
+    # 3 LSTM layers
+    x = CuDNNLSTM(units, kernel_initializer=kernel_init_rnn, bias_initializer=bias_init_rnn, unit_forget_bias=True, return_sequences=True, name='CuDNN_lstm1')(x)
+    x = CuDNNLSTM(units, kernel_initializer=kernel_init_rnn, bias_initializer=bias_init_rnn, unit_forget_bias=True, return_sequences=True, name='CuDNN_lstm2')(x)
+    x = CuDNNLSTM(units, kernel_initializer=kernel_init_rnn, bias_initializer=bias_init_rnn, unit_forget_bias=True, return_sequences=True, name='CuDNN_lstm3')(x)
 
     # 1 fully connected relu layer
     x = TimeDistributed(Dense(units=units, kernel_initializer=kernel_init_dense, bias_initializer=bias_init_dense,
