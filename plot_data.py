bf4f6ef81f54598e01423c04b12d1913b37fb3bd
anitakau <anitakau@stud.ntnu.no>
2018-05-18 12:03:35 +0200

    Plot data to graph

--- plot_data.py
+++ plot_data.py
@@ -1,29 +1,41 @@
 import matplotlib.pyplot as plt
 import csv
 
-path = ""       #path to csv file
-y_max = 500     # height y axis
+filename = ""                       # Name of file
+path = "" + filename + ".csv"       # Path to save image of graph
+savepath = "" + filename + ".png"   # Name to save file
+
+title = ""                          # Unique model name
+y_max = 300                         # height y axis
 
 val_loss = []
 loss = []
+wer = []
 
 with open(path) as csvfile:
     csv_reader = csv.reader(csvfile)
     for row in csv_reader:
         loss.append(row[1])
         val_loss.append(row[2])
+        wer.append(row[3])
 
 loss_temp = []
 val_temp = []
+wer_temp = []
 
-plt.xlim(0, len(loss))
-plt.ylim(0, y_max)
+#plt.xlim(0, 70)
+#plt.ylim(0, y_max)
 
 for i in range(1, len(loss)):
-    loss_temp.append(loss[i])
-    val_temp.append(val_loss[i])
-
-plt.plot(loss_temp)
-plt.plot(val_temp)
-
+    loss_temp.append(float(loss[i]))
+    val_temp.append(float(val_loss[i]))
+    wer_temp.append(float(wer[i])*100)
+plt.xlabel("Epochs")
+plt.title(title)
+plt.plot(loss_temp, label="loss", color='blue',linewidth=2)
+plt.plot(val_temp, label="val_loss", color='red', linewidth=2)
+plt.plot(wer_temp, label="wer", color='black', linewidth=2)
+plt.grid()
+plt.legend()
+plt.savefig(savepath)
 plt.show()
