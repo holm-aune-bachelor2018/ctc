33b1198fc7725f6a1dac44c1d244ec880ea6d161
Marit Sundet Holm <marith1@stud.ntnu.no>
2018-06-01 13:07:10 +0200

    Added license

--- char_map.py
+++ char_map.py
@@ -1,6 +1,16 @@
-# From Baidu ba-dls-deepspeech - https://github.com/baidu-research/ba-dls-deepspeech
-# Character map list
+"""
+This file is taken from
+Baidu ba-dls-deepspeech - https://github.com/baidu-research/ba-dls-deepspeech
 
+Which is under the Apache License:
+    Copyright 2015-2016 Baidu USA LLC.  All rights reserved.
+
+    Apache License
+    Version 2.0, January 2004
+    http://www.apache.org/licenses/
+"""
+
+# Character map list
 
 char_map_str = """
 <SPACE> 0
