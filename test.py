51dfb185d1d62a706de0b4cd4c13d5ff2b514923
Marit Sundet Holm <marith1@stud.ntnu.no>
2018-06-02 12:24:57 +0200

    Changed tests to use sample data

---
+++ test.py
@@ -0,0 +1,104 @@
+"""
+LICENSE
+
+This file is part of Speech recognition with CTC in Keras.
+The project is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+version.
+The project is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with this project.
+If not, see http://www.gnu.org/licenses/.
+
+"""
+
+import argparse
+from datetime import datetime
+
+import keras.backend as K
+import tensorflow as tf
+from keras import models
+from keras.callbacks import ReduceLROnPlateau, ModelCheckpoint, EarlyStopping
+from keras.optimizers import Adam
+from keras.utils import multi_gpu_model
+
+import nn_models
+from DataGenerator import DataGenerator
+from LossCallback import LossCallback
+from data import combine_all_wavs_and_trans_from_csvs
+
+
+import unittest
+
+
+class TestModelCompile(unittest.TestCase):
+    def setUp(self):
+        self.optimizer = Adam(lr=0.0001, epsilon=1e-8, clipnorm=2.0)
+        self.loss = {'ctc': lambda y_true, y_pred: y_pred}
+        # Do stuff
+
+    def tearDown(self):
+        K.clear_session()
+
+    def test_brnn_compile(self):
+        try:
+            model = nn_models.brnn(units=256, input_dim=26, output_dim=29, dropout=0.2, numb_of_dense=3)
+            model.compile(loss=self.loss, optimizer=self.optimizer)
+        except ():
+            model = None
+
+        self.assertIsNotNone(model)
+
+    def test_deep_rnn_compile(self):
+        try:
+            model = nn_models.deep_rnn(units=256, input_dim=26, output_dim=29, dropout=0.2, numb_of_dense=3, n_layers=3)
+            model.compile(loss=self.loss, optimizer=self.optimizer)
+        except ():
+            model = None
+
+        self.assertIsNotNone(model)
+
+    def test_blstm_compile(self):
+        try:
+            model = nn_models.blstm(units=256, input_dim=26, output_dim=29, dropout=0.2, numb_of_dense=3, cudnn=False,
+                                    n_layers=1)
+            model.compile(loss=self.loss, optimizer=self.optimizer)
+        except ():
+            model = None
+
+        self.assertIsNotNone(model)
+
+    def test_deep_lstm_compile(self):
+        try:
+            model = nn_models.deep_lstm(units=256, input_dim=26, output_dim=29, dropout=0.2, numb_of_dense=3,
+                                        cudnn=False, n_layers=3)
+            model.compile(loss=self.loss, optimizer=self.optimizer)
+        except ():
+            model = None
+
+        self.assertIsNotNone(model)
+
+    def test_cnn_blstm_compile(self):
+        try:
+            model = nn_models.cnn_blstm(units=256, input_dim=26, output_dim=29, dropout=0.2, seq_padding=2048,
+                                        cudnn=False, n_layers=1)
+            model.compile(loss=self.loss, optimizer=self.optimizer)
+        except ():
+            model = None
+
+        self.assertIsNotNone(model)
+
+    def test_train(self):
+        sample_data="data_dir/sample_librivox-test-clean.csv"
+        _, df = combine_all_wavs_and_trans_from_csvs(sample_data)
+        data_generator = DataGenerator(df, feature_type='mfcc', batch_size=6, frame_length=320, hop_length=160,
+                                       n_mels=40, mfcc_features=26, epoch_length=0, shuffle=True)
+        model = nn_models.brnn(units=256, input_dim=26, output_dim=29, dropout=0.2, numb_of_dense=3)
+        model.compile(loss=self.loss, optimizer=self.optimizer)
+
+        # Run training
+        model.fit_generator(generator=data_generator, epochs=1, verbose=0)
+
+
+if __name__ == '__main__':
+    unittest.main()
